13c02724689eebd18bdc5a6493e23f8f83b1064c
Jasper <jasper.avrack@gmail.com>
2015-12-11 19:18:35 +0100

    Staat nu in root van onedrive
    Staat nu in root van onedrive

--- management.py
+++ management.py
@@ -16,8 +16,8 @@ client = SoapClient(
     ns = False)
 
 #HTML-metadata I guess??
-print 'Status: 200 OK'
-print 'Content-type: text/html'
+print "Content-Type: text/html;charset=utf-8"
+print
 
 #De default pagina
 print '<HTML><HEAD><TITLE>Welkom bij dit management script ofzo enzo!</TITLE></HEAD>'
@@ -28,23 +28,25 @@ print '<p>'
 print 'Hier komt uitleg over de knoppen...'
 print '<br>'
 
-print '</BODY>'
+
 
 # Code om zooi op te vragen
-#print "Resultaten:"
+print "Resultaten:"
 # call a few remote methods
-#r1=str(client.get_value(number=1).resultaat)
-#print "Resultaat number=1 :", r1
+r1=str(client.get_value(number=1).resultaat)
+print "Resultaat number=1 :", r1
 #
-#r2=str(client.get_value(number=2).resultaat)
-#print "Resultaat number=2 :", r2
+r2=str(client.get_value(number=2).resultaat)
+print "Resultaat number=2 :", r2
 #
-#r3=str(client.get_value(number=3).resultaat)
-#print "Resultaat number=3 :", int(r3) # r3 is a number!
+r3=str(client.get_value(number=3).resultaat)
+print "Resultaat number=3 :", int(r3) # r3 is a number!
 #
-#r4=str(client.get_value(number=4).resultaat)
-#print "Resultaat number=4 :", r4.rstrip() # This is a multiline: strip the newline from the result!
+r4=str(client.get_value(number=4).resultaat)
+print "Resultaat number=4 :", r4.rstrip() # This is a multiline: strip the newline from the result!
 #
-#r5=str(client.get_value(number=5).resultaat)
-#print "Resultaat number=5 :", r5.rstrip()
+r5=str(client.get_value(number=5).resultaat)
+print "Resultaat number=5 :", r5.rstrip()
+print '</BODY>'
+print '</html>'
 
