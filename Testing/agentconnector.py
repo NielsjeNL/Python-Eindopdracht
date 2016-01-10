125715883a54d5f397bef7069a08ff3bc5c75647
Jasper <jasper.avrack@gmail.com>
2016-01-10 16:28:16 +0100

    Logging toegevoegd en controle of server online is bij uitvoeren taak
    Logging toegevoegd aan management.py en controle toegevoegd aan het
    ophalen van gegevens of apparaat wel online is.

--- Testing/agentconnector.py
+++ Testing/agentconnector.py
@@ -4,7 +4,6 @@ Beschrijving van bestand
 
 """
 from pysimplesoap.client import SoapClient
-from lxml import etree
 
 class agent():
     ''' Class agent waarin de agentobjecten worden aangemaakt '''  
@@ -17,10 +16,11 @@ class agent():
                                  namespace = "http://example.com/sample.wsdl",
                                  soap_ns='soap',
                                  ns = False)
+        self.online = self.checkconnection()
         print 'agent aangemaakt:',self.name, self.host,self.port
-        print 'verbinding check', self._checkconnection()
+        print 'verbinding check', self.online
     
-    def _checkconnection(self):
+    def checkconnection(self):
         '''Simpele test of dat de verbinding tot stand kan worden gebracht.'''
         try:
             self.client.Echo(value=True)
@@ -30,8 +30,13 @@ class agent():
             
     def retrievedata(self, platform=False, ip=False, loggedinusers=False, services=False, freespace=False, ram=False, uptime=False):
         '''Functie om gegevens op te vragen aan de server. Er kan worden opgegeven welke waarde er moet worden opgevraagd door dat argument al True mee te geven'''
-        response = self.client.get_value(platform=platform, ip=ip, loggedinusers=loggedinusers, services=services, freespace=freespace, ram=ram, uptime=uptime).resultaat
-        return response
+        try:
+            response = self.client.get_value(platform=platform, ip=ip, loggedinusers=loggedinusers, services=services, freespace=freespace, ram=ram, uptime=uptime).resultaat
+            self.online = True            
+            return response
+        except:
+            self.online = False
+            return False
 
 #test = agent('Jasper-PC1', 'localhost', 8008)
 #reactie = test.retrievedata(platform=True, ip=True, loggedinusers=True, services=True, freespace=True, ram=True, uptime=True)
--- Testing/management.py
+++ Testing/management.py
@@ -5,14 +5,30 @@ Created on Sun Jan 10 02:08:36 2016
 @author: jaspe
 """
 from lxml import etree
-from agentconnector import *
+from agentconnector import agent
+import logging
 
-# Config inlezen
+#-----------------------
+#configuratie inladen
 configtree = etree.parse('mgmtconfig.xml')
-#hostname = str(configtree.xpath('/config/hostname/text()')[0])
-#port = str(configtree.xpath('/config/port/text()')[0])
+# Logging opzetten
+logfile = str(configtree.xpath('/config/logging/logfile/text()')[0])
+loglevel = str(configtree.xpath('/config/logging/loglevel/text()')[0])
+logger = logging.getLogger()
+logger.setLevel(loglevel)
+# file handler maken, deze schrijft uiteindelijk het log weg
+handler = logging.FileHandler(logfile)
+handler.setLevel(loglevel)
+# format voor het log
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+handler.setFormatter(formatter)
+# handler toevoegen aan het log, let op dat je dit maar 1 keer doet anders krijg je dubbele logs
+logger.addHandler(handler)
+# begin van het log schrijven
+logger.warning('--------------BEGIN LOG--------------')
+logger.warning('Script is begonnen')  
 
-# Loop door alle boeken...
+# Loop door alle servers...
 systems = configtree.xpath('/config/server')
 serveragent={}
 for i in systems:
@@ -20,3 +36,10 @@ for i in systems:
     host = i.xpath('./host')[0].text
     port = i.xpath('./port')[0].text
     serveragent[i.get('name')] = agent(name, host, port)
+    logger.warning('agent aangemaakt: '+serveragent[i.get('name')].name+' '+serveragent[i.get('name')].host+' '+serveragent[i.get('name')].port)
+    logger.warning('verbinding check '+str(serveragent[i.get('name')].online))
+
+
+logger.warning('---------------END LOG---------------')
+handler.close()
+logger.removeHandler(handler)
