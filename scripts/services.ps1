0a1af6e275b6f721f97fa78d4906a0328505923a
Jasper <jasper.avrack@gmail.com>
2016-01-11 19:02:42 +0100

    Bug fix
    script gaf twee keer de waarde van running service terug ipv running en
    stopped

--- scripts/services.ps1
+++ scripts/services.ps1
@@ -3,6 +3,6 @@
 $stopped = (Get-Service | Where-Object { $_.Status -eq "Stopped" }).length
 $running = (Get-Service | Where-Object { $_.Status -eq "Running" }).length
 
-write-host -NoNewline $running $running
+write-host -NoNewline $running $stopped
 }
 Get-Services
